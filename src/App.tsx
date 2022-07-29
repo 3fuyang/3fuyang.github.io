13aa6709aeda63e69fd4485740e56d3f411621cf
fwio <624835339@qq.com>
2022-07-29 23:35:43 +0800

    Add eslint for code formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
--- uno.config.ts
+++ uno.config.ts
@@ -1,19 +1,17 @@
 // Splitted from vite.config.ts, only to make the VSCode extension for UnoCSS work.
-import { defineConfig } from "unocss"
+import { defineConfig } from 'unocss'
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
 
 export default defineConfig({
-  shortcuts: [
-    { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
-  ],
+  shortcuts: [],
   presets: [
     presetUno(),
     presetAttributify(),
     presetIcons({
       extraProperties: {
         'display': 'inline-block',
-        'vertical-align': 'middle',
-      },
-    }),
+        'vertical-align': 'middle'
+      }
+    })
   ]
 })
