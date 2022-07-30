54f415f5bb13f3d54d78b2f9b94d36c846c7a843
fwio <624835339@qq.com>
2022-07-30 17:32:50 +0800

    Remove prettier, only use ESLint for formatting and checking,

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App () {
   return (
     <div className='text-center'>
       <header className='bg-#282c34 min-h-100vh flex flex-col items-center justify-center color-white'>
-        <div className='logo' />
+        <div className="logo" />
         <h1 className='mt-2em animate-bounce-alt animate-duration-2s'>
           Hello Vite + React!
         </h1>
