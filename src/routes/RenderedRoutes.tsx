7f3a64a7d8b078e46c751d3212e030df67a45825
fwio <624835339@qq.com>
2022-08-04 21:12:20 +0800

    Wrap the output MDX with <article> element.

--- src/routes/RenderedRoutes.tsx
+++ src/routes/RenderedRoutes.tsx
@@ -1,7 +1,11 @@
-import { FC } from 'react'
+import { createElement, FC } from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from '~react-pages'
 
-const RenderedRoutes:FC = () => useRoutes(routes)
+routes.forEach((route) => {
+  route.element = createElement('article', { className: 'prose' }, route.element)
+})
+
+const RenderedRoutes:FC = () =>  useRoutes(routes)
 
 export default RenderedRoutes
