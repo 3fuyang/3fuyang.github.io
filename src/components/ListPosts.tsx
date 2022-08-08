24e6736dbecb1b804988bc4a53b9b79b64694ee1
fwio <624835339@qq.com>
2022-08-08 18:49:14 +0800

    Enable the essay folder.

--- src/components/ListPosts.tsx
+++ src/components/ListPosts.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Frontmatter } from '../../types/frontmatter'
 import { useFrontmatter } from '../hooks/useFrontmatter'
 
-export default function Post() {
+export default function ListPosts() {
   const [frontmatters, setFrontmatters] = useState<Frontmatter[]>([])
 
   useFrontmatter(setFrontmatters)
@@ -15,7 +15,7 @@ export default function Post() {
           <Link
             to={frontmatter.path}
             className="block box-border rounded-sm op70 dark:op80 hover:op100 focus:op100 ml-4 p-1 hover:drop-shadow focus:drop-shadow"
-            border="r-4 #878584 hover:blue-400 dark:hover:blue-5"
+            border="r-4 #878584 hover:blue-4 dark:hover:blue-5 focus:blue-4 dark:focus:blue-5"
             flex="~ col"
             transition="~">
             <span className="text-3xl font-bold">{frontmatter.title}</span>
