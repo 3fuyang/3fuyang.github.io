9d8cf3993fb79434a6b3ce318e579959fe8e0e00
3fuyang <3fuyang@users.noreply.github.com>
2022-10-11 08:30:41 +0000

    deploy: 9354307ccdb5cbc0356481236fb88dfafab36793

--- assets/index.0c1652ac.js
+++
@@ -1,1 +0,0 @@
-import"./ListPosts.e2f8d10f.js";import{L as a}from"./ListPosts.e2f8d10f.js";import"./index.fa5ba3ad.js";export{a as default};
---
+++ assets/index.f0ad0c20.js
@@ -0,0 +1,16 @@
+import{a as l,j as n,F as a}from"./index.9c774f50.js";const c="Fwio",t="<1min",s="zh",h="2022-08-17T23:04:00.000+00:00",d="/";function r(i){const e=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",h1:"h1",span:"span",p:"p",ul:"ul",strong:"strong"},i.components);return n(a,{children:[l(e.nav,{className:"toc",children:l(e.ol,{className:"toc-level toc-level-1",children:l(e.li,{className:"toc-item toc-item-h1",children:l(e.a,{className:"toc-link toc-link-h1",href:"#hi-there-",children:"Hi there \u{1F44B}"})})})}),`
+`,n(e.h1,{id:"hi-there-",children:["Hi there ",l(e.span,{role:"img","aria-label":"waving hand",children:"\u{1F44B}"})]}),`
+`,l(e.p,{children:"This is my personal blog, basically including 4 modules:"}),`
+`,n(e.ul,{children:[`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"bookmark tabs",children:"\u{1F4D1}"})," ",l(e.strong,{children:"Blog"})," - Some technical articles."]}),`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"notebook",children:"\u{1F4D3}"})," ",l(e.strong,{children:"Notes"})," - Notes about vanilla JS APIs."]}),`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"pencil",children:"\u270F\uFE0F"})," ",l(e.strong,{children:"Essay"})," - Yep, just essays."]}),`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"laptop",children:"\u{1F4BB}"})," ",l(e.strong,{children:"Projects"})," - Simple introduction of my projects."]}),`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"keyboard",children:"\u2328\uFE0F"})," ",l(e.strong,{children:"Demos"})," - Also projects, but much smaller and simpler."]}),`
+`]}),`
+`,l("br",{}),`
+`,n(e.ul,{children:[`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"seedling",children:"\u{1F331}"})," Currently, I'm learning ",l(e.a,{href:"https://programmercarl.com/",children:"ProgrammerCarl"}),"."]}),`
+`,n(e.li,{children:[l(e.span,{role:"img","aria-label":"e-mail",children:"\u{1F4E7}"})," How to reach me: ",l(e.a,{href:"mailto:624835339@qq.com",children:"624835339@qq.com"})]}),`
+`]}),`
+`]})}function m(i={}){const{wrapper:e}=i.components||{};return e?l(e,Object.assign({},i,{children:l(r,i)})):r(i)}export{h as date,m as default,t as duration,s as lang,d as path,c as title};
