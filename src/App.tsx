ca8721e20d2ecc23d6e2a6cb161430d9c4e81765
fwio <624835339@qq.com>
2022-12-26 22:34:37 +0800

    suspense: remove unnecessary lazy imports and <Suspense />

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense, useEffect, useMemo } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import ErrorBoundary from './components/ErrorBoundary'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+//import Loading from './components/Loading'
+import Skeleton from './components/Skeleton'
 
-const ErrorBoundary = lazy(() => import('./components/ErrorBoundary'))
-const NavBar = lazy(() => import('./components/NavBar'))
-const Footer = lazy(() => import('./components/Footer'))
-const Loading = lazy(() => import('./components/Loading'))
-const Skeleton = lazy(() => import('./components/Skeleton'))
 const RenderedRoutes = lazy(() => import('./routes/RenderedRoutes'))
 
 function App() {
@@ -44,18 +44,14 @@ function App() {
   return (
     <ErrorBoundary>
       <Router>
-        <Suspense fallback={<Loading msg="Initializaing ..." />}>
-          <NavBar />
-          <Suspense fallback={<Loading msg="Loading ..." />}>
-            <main className="px7 py10">
-              <Suspense fallback={<Skeleton type="text" />}>
-                <RenderedRoutes />
-              </Suspense>
-              <Footer />
-            </main>
-            <img aria-hidden="true" src="/seagull.svg" alt="seagull" className="fixed top-1/2 left-1/2 -translate-1/2 max-w-2/5 md:max-w-1/6 ha aspect-1 op45 dark:op100 filter-invert-60 mix-blend-difference pointer-events-none" />
+        <NavBar />
+        <main className="px7 py10">
+          <Suspense fallback={<Skeleton type="text" />}>
+            <RenderedRoutes />
           </Suspense>
-        </Suspense>
+          <Footer />
+        </main>
+        <img aria-hidden="true" src="/seagull.svg" alt="seagull" className="fixed top-1/2 left-1/2 -translate-1/2 max-w-2/5 md:max-w-1/6 ha aspect-1 op45 dark:op100 filter-invert-60 mix-blend-difference pointer-events-none" />
       </Router>
     </ErrorBoundary>
   )
