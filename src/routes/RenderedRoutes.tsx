197f38cafd7322635b8fa57663dd370ea11e20b2
fwio <624835339@qq.com>
2023-01-02 17:40:39 +0800

    routing: make the automatic routing compile-time

--- src/routes/RenderedRoutes.tsx
+++ src/routes/RenderedRoutes.tsx
@@ -1,29 +1,19 @@
-import { createElement, FC, memo, lazy } from 'react'
-import { RouteObject, useRoutes } from 'react-router-dom'
-import PostWrapper from '../components/PostWrapper'
-import manualRoutes from './auto-routes'
+import { memo, lazy } from 'react'
+import { useRoutes } from 'react-router-dom'
+//import manualRoutes from './auto-routes'
+import autoRoutes from '../models/routes'
 
 const NotFound = lazy(() => import('../components/NotFound'))
 
-function parseRoutes(routes: RouteObject[]) {
-  routes.forEach((route) => {
-    if (route.element && route.path?.length) {
-      // markdown file, wrap it
-      route.element = createElement(PostWrapper, { path: route.path as string }, route.element)
-    } else if (route.children) {
-      // folder, handle it recursively
-      parseRoutes(route.children)
-    }
-  })
-
-  routes.push({
+const fullRoutes = [
+  ...autoRoutes,
+  {
     path: '*',
     element: <NotFound />
-  })
+  }
+]
 
-  return routes
-}
 
-const RenderedRoutes = () => useRoutes(parseRoutes(manualRoutes))
+const RenderedRoutes = () => useRoutes(fullRoutes)
 
 export default memo(RenderedRoutes)
