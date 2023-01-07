909b2cd5377134360ee299ba3ac8ccbb4825743e
fwio <624835339@qq.com>
2023-01-07 17:10:06 +0800

    fix: update the bird logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import type { FLinkProps } from './FLink'
 import { GoMarkGithub } from 'react-icons/go'
 import { AiOutlineFileText } from 'react-icons/ai'
-import { GiBookshelf, GiSeagull, GiNotebook } from 'react-icons/gi'
+import { GiBookshelf, GiEgyptianBird, GiNotebook } from 'react-icons/gi'
 import { MdAppRegistration, MdOutlineAlbum } from 'react-icons/md'
 import FLink from './FLink'
 import ToggleTheme from './ToggleTheme'
@@ -40,7 +40,7 @@ const NavBar: FC = () => {
   return (
     <header className="flex px5 py3 items-center sticky top-0 z1 backdrop-filter backdrop-blur backdrop-saturate-50">
       <Link to="/" className="select-none outline-none" title="Fwio">
-        <GiSeagull className="h-10 wa color-cyan-9 dark:color-#83BEEC" aria-hidden />
+        <GiEgyptianBird className="h-10 wa color-cyan-9 dark:color-cyan-6" aria-hidden />
       </Link>
       <div className="spacer flex-1" />
       <nav flex="~ gap-6 md:gap-8 items-center">
