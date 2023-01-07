9fd1071b70b374edaac7b16c6ea4e2348bc16612
fwio <624835339@qq.com>
2023-01-07 17:11:11 +0800

    fix: remove ErrorBoundary to enable react's fast-fresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense, useEffect, useMemo } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import ErrorBoundary from './components/ErrorBoundary'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 //import Loading from './components/Loading'
@@ -42,18 +41,16 @@ function App() {
   }, [])
 
   return (
-    <ErrorBoundary>
-      <Router>
-        <NavBar />
-        <main className="px7 py10">
-          <Suspense fallback={<Skeleton type="text" />}>
-            <RenderedRoutes />
-          </Suspense>
-          <Footer />
-        </main>
-        <img aria-hidden="true" src="/seagull.svg" alt="seagull" className="fixed top-1/2 left-1/2 -translate-1/2 max-w-2/5 md:max-w-1/6 ha aspect-1 op45 dark:op100 filter-invert-60 mix-blend-difference pointer-events-none" />
-      </Router>
-    </ErrorBoundary>
+    <Router>
+      <NavBar />
+      <main className="px7 py10">
+        <Suspense fallback={<Skeleton type="text" />}>
+          <RenderedRoutes />
+        </Suspense>
+        <Footer />
+      </main>
+      <img aria-hidden="true" src="/seagull.svg" alt="seagull" className="fixed top-1/2 left-1/2 -translate-1/2 max-w-2/5 md:max-w-1/6 ha aspect-1 op45 dark:op100 filter-invert-60 mix-blend-difference pointer-events-none" />
+    </Router>
   )
 }
 
