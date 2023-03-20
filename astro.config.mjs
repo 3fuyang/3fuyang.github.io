6ee33937a2f0c2ffb094d79b931b1c97363c6a72
fwio <624835339@qq.com>
2023-03-20 21:49:44 +0800

    feat(shiki): use `vitesse-dark` as theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import UnoCSS from '@unocss/astro'
 
+import vitesseDark from './src/styles/vitesse-dark.json'
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: vitesseDark,
     },
   },
   integrations: [react(), UnoCSS()],
