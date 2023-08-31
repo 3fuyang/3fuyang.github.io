828a2934f3c89171771626034ca222b5c5cf8cc7
fwio <624835339@qq.com>
2023-09-01 02:06:15 +0800

    style: optimized toggle component

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,35 +1,36 @@
-import { FC, useCallback, useMemo, memo } from 'react'
-// import { MdLightMode, MdDarkMode } from 'react-icons/md'
-import { useDark } from '../hooks/useDark'
+import { useCallback, useMemo, memo } from 'react'
 
-const ToggleTheme: FC = () => {
+import { THEME, THEME_STORAGE_KEY, useDark } from '../hooks/useDark'
+
+const darkIcon = <div className="i-material-symbols-dark-mode w5 h5 text-white" />
+const lightIcon = <div className="i-material-symbols-light-mode w5 h5" />
+
+function ToggleTheme() {
   const [mode, setMode] = useDark()
 
   const toggleTheme = useCallback(() => {
-    const newMode = mode === 'dark' ? 'light' : 'dark'
-    localStorage.setItem('theme', newMode)
-    setMode(newMode)
-  }, [mode, setMode])
+    setMode(m => {
+      const updatedMode = m === THEME.dark ? THEME.light : THEME.dark
+      localStorage.setItem(THEME_STORAGE_KEY, updatedMode)
+      return updatedMode
+    })
+  }, [mode])
 
-  const [ToggleTitle, ToggleIcon] = useMemo(() => {
+  const [toggleTitle, toggleIcon] = useMemo(() => {
     return [
-      mode === 'dark' ? 'Light' : 'Dark',
-      mode === 'dark' ? (
-        <div className="i-material-symbols-dark-mode w5 h5 text-white" />
-      ) : (
-        <div className="i-material-symbols-light-mode w5 h5" />
-      ),
+      mode === THEME.dark ? THEME.light : THEME.dark,
+      mode === THEME.dark ? darkIcon : lightIcon,
     ]
   }, [mode])
 
   return (
-    <div
-      className="select-none link cursor-pointer"
-      title={`Toggle to ${ToggleTitle} Mode`}
+    <button
+      className="link"
+      title={`Toggle to ${toggleTitle} mode`}
       onClick={toggleTheme}
     >
-      {ToggleIcon}
-    </div>
+      {toggleIcon}
+    </button>
   )
 }
 
--- src/hooks/useDark.tsx
+++ src/hooks/useDark.tsx
@@ -1,23 +1,34 @@
 import { useEffect, useState } from 'react'
 
+export type Theme = 'dark' | 'light'
+
+export const THEME = {
+  dark: 'dark',
+  light: 'light',
+} as const satisfies Record<Theme, string>
+
+export const DEFAULT_THEME = THEME.light satisfies Theme
+
+export const THEME_STORAGE_KEY = 'theme'
+
 export function useDark() {
-  const [theme, setTheme] = useState<'dark' | 'light'>('light')
+  const [theme, setTheme] = useState<Theme>(DEFAULT_THEME)
 
   // on load
   useEffect(() => {
-    const prevTheme = localStorage.getItem('theme')
+    const prevTheme = localStorage.getItem(THEME_STORAGE_KEY)
     if (
-      prevTheme === 'dark' ||
+      prevTheme === THEME.dark ||
       (!prevTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
-      setTheme('dark')
+      setTheme(THEME.dark)
     }
   }, [])
 
   // on re-render
   useEffect(() => {
     const root = document.documentElement
-    if (theme === 'light') {
+    if (theme === THEME.light) {
       root.classList.remove('dark')
     } else {
       root.classList.add('dark')
