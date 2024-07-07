aedeb623101884482b44e28be9eae5b382ef8b87
fwio <624835339@qq.com>
2024-07-08 00:36:58 +0800

    feat(posts): addded autolink and external link plugins

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import { defineConfig } from 'astro/config'
 import { presetTypography } from 'unocss'
 import UnoCSS from 'unocss/astro'
 import vercelStatic from '@astrojs/vercel/static'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +15,25 @@ export default defineConfig({
     shikiConfig: {
       theme: 'vitesse-dark',
     },
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          properties: {
+            className: 'prose-heading-anchor',
+          },
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['nofollow', 'noopener', 'noreferrer'],
+        },
+      ],
+    ],
   },
   integrations: [
     react(),
